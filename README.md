c7e22bdcb7960fd18085f68891ed8ebd0de6ed78
Z4nna <84254228+Z4nna@users.noreply.github.com>
2024-03-22 11:41:43 +0100

    updated README to include the installation script instructions, and updated the installation script to add minimal error handling and modularity.

--- README.md
+++ README.md
@@ -8,15 +8,15 @@ Inside the src folder there are two packages:
 2. nereo_interfaces
     This package contains all the custom messages and services provided by the ROV.
 ## Setup instructions
-### I will do an installation script to automatically setup the workspace and the packages, but as of now you'll have to do it manually.
+### Option 1: manual installation
 1. Create a ROS2 workspace: navigate to your desired folder and run:
-    ```
+    ```bash
     mkdir -p ros2_ws/src
     cd ros2_ws
     colcon build
     ```
 2. Create the packages, in the root folder of the workspace run:
-    ```
+    ```bash
     cd src
     ros2 pkg create --build-type ament_python nereo_pkg
     ros2 pkg create nereo_interfaces
@@ -27,11 +27,15 @@ Inside the src folder there are two packages:
     Now copy the folders R"nereo_pkg/launch" and R"nereo_pkg/nereo_pkg" inside your newly created python package (W"src/nereo_pkg"), as well as the R"nereo_pkg/package.xml" and R"nereo_pkg/setup.py" files. Replace the already existing files, if any.
     Now copy the folders R"nereo_interfaces/msg" and R"nereo_interfaces/srv" inside your newly created cmake package (W"src/nereo_interfaces"), as well as the R"nereo_interfaces/package.xml" and R"nereo_interfaces/CMakeLists.txt" files. Replace the already existing files, if any. Inside W"src/nereo_interfaces/", make sure to delete the folders "include" and "src".
 4. Move to the root directory of your workspace and run:
-    ```
+    ```bash
     colcon build
     ```
     All the packages should now be built, make sure to include them in your .bashrc file:
-    ```
+    ```bash
     echo source install/setup.bash > ~/.bashrc ## this line is optional, it will make sure that the workspace is automatically loaded every time you open a new terminal.
     source install/setup.bash
     ```
+### Option 2: installation script
+1. Clone this repo in the location you desire your ros2_ws to be located.
+2. execute the installation script ("nome")
+3. Done, you can now delete the cloned repo!
--- nereo_ros_package_installer.sh
+++ nereo_ros_package_installer.sh
@@ -1,43 +1,103 @@
 #!/bin/bash
-echo "Welcome to the installer of Nereo's ROS package!"
-read -p "Would you like to proceed?"
-read -p "Enter the name of the cloned repository: " repo_name
-source /opt/ros/humble/setup.bash
-cd ..
-mkdir -p ros2_nereo_ws/src
-cd ros2_nereo_ws
-colcon build
-# creates packages
-cd src
-ros2 pkg create --build-type ament_python nereo_pkg
-ros2 pkg create nereo_interfaces
-mkdir -p nereo_pkg/launch
-# adds nereo_pkg code
-cp -afv ../../$repo_name/src/nereo_pkg/launch/. ./nereo_pkg/launch/
-cp -afv ../../$repo_name/src/nereo_pkg/nereo_pkg/. ./nereo_pkg/nereo_pkg/
-cp -afv ../../$repo_name/src/nereo_pkg/package.xml ./nereo_pkg/
-cp -afv ../../$repo_name/src/nereo_pkg/setup.py ./nereo_pkg/
-# adds nereo_interfaces code
-cp -afv ../../$repo_name/src/nereo_interfaces/msg/. ./nereo_interfaces/msg/
-cp -afv ../../$repo_name/src/nereo_interfaces/srv/. ./nereo_interfaces/srv/
-cp -afv ../../$repo_name/src/nereo_interfaces/package.xml ./nereo_interfaces/
-cp -afv ../../$repo_name/src/nereo_interfaces/CMakeLists.txt ./nereo_interfaces/
-rm -rfv ./nereo_interfaces/include
-rm -rfv ./nereo_interfaces/src
-# builds and sources the packages
-cd ..
-colcon build
-source ./install/setup.bash
-read -p "Would you like to permanently source the setup script? You won't have to source it again for every terminal you open. (YES / no): " source_setup
-if [ $source_setup == ""];
-then
-	$source_setup = "YES"
-fi
-if [ $source_setup == "YES" ];
-then
-	echo source ./install/setup.bash >> ~/.bashrc
-fi
-echo "Nereo packages are successfully installed!"
-echo "You can now delete the previously cloned repository."
-read -p "Press any key to exit."
 
+# Function to check if the command executed successfully
+check_success() {
+    if [ $? -ne 0 ]; then
+        echo "Error: $1 failed. Exiting."
+        exit 1
+    fi
+}
+
+# Function to create ROS packages
+create_ros_packages() {
+    ros2 pkg create --build-type ament_python nereo_pkg
+    ros2 pkg create nereo_interfaces
+}
+
+# Function to copy code and files from cloned repository
+copy_code_from_repo() {
+    cp -afv ../$repo_name/src/nereo_pkg/launch/. ./src/nereo_pkg/launch/
+    cp -afv ../$repo_name/src/nereo_pkg/nereo_pkg/. ./src/nereo_pkg/nereo_pkg/
+    cp -afv ../$repo_name/src/nereo_pkg/package.xml ./src/nereo_pkg/
+    cp -afv ../$repo_name/src/nereo_pkg/setup.py ./src/nereo_pkg/
+
+    cp -afv ../$repo_name/src/nereo_interfaces/msg/. ./src/nereo_interfaces/msg/
+    cp -afv ../$repo_name/src/nereo_interfaces/srv/. ./src/nereo_interfaces/srv/
+    cp -afv ../$repo_name/src/nereo_interfaces/package.xml ./src/nereo_interfaces/
+    cp -afv ../$repo_name/src/nereo_interfaces/CMakeLists.txt ./src/nereo_interfaces/
+    rm -rfv ./src/nereo_interfaces/include
+    rm -rfv ./src/nereo_interfaces/src
+}
+
+# Function to build ROS packages
+build_ros_packages() {
+    colcon build
+}
+
+# Function to source ROS setup script
+source_ros_setup() {
+    source ./install/setup.bash
+}
+
+# Function to add setup script to .bashrc
+add_to_bashrc() {
+    echo "source ./install/setup.bash" >> ~/.bashrc
+}
+
+# Function to display installation completion message
+display_completion_message() {
+    echo "Nereo packages are successfully installed!"
+    echo "If encountering any error during usage, check the permissions :)"
+    echo "You can now delete the previously cloned repository."
+    read -p "Press any key to exit."
+}
+
+echo "Welcome to the installer of Nereo's ROS package!"
+
+# Validate user input for proceeding
+read -p "Would you like to proceed? (yes/no): " proceed
+if [[ ! $proceed =~ ^[Yy][Ee][Ss]|[Yy]$ ]]; then
+    echo "Installation aborted."
+    exit 0
+fi
+
+# Validate user input for repository name
+read -p "Enter the name of the cloned repository: " repo_name
+if [ ! -d "../$repo_name" ]; then
+    echo "Error: Repository not found. Make sure you have entered the correct name."
+    exit 1
+fi
+
+# Source ROS setup script
+source /opt/ros/humble/setup.bash
+check_success "Sourcing ROS setup script"
+
+# Create ROS workspace and packages
+mkdir -p ../ros2_nereo_ws/src
+cd ../ros2_nereo_ws
+colcon build
+cd src
+create_ros_packages
+check_success "Creating ROS packages"
+
+# Copy code from cloned repository
+cd ..
+copy_code_from_repo
+check_success "Copying code from repository"
+
+# Build ROS packages
+build_ros_packages
+check_success "Building ROS packages"
+
+# Source ROS setup script
+source_ros_setup
+
+# Ask user to add setup script to .bashrc
+read -p "Would you like to permanently source the setup script? You won't have to source it again for every terminal you open. (yes/no): " source_setup
+if [[ $source_setup =~ ^[Yy][Ee][Ss]|[Yy]$ ]]; then
+    add_to_bashrc
+    echo "Setup script added to .bashrc."
+fi
+
+# Display completion message
+display_completion_message
